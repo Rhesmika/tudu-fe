c76afd3772a6ed9d551b1ff9d46cfd202b19c29c
Rhesmika <Rhesmika@gmail.com>
2023-02-21 13:21:29 +0000

    add app css import logo and favicon. create navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/tudu_logo.png";
+import styles from "../styles/NavBar.module.css";
+
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img className={styles.Logo} src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <Nav.Link>
+                <i class="fa-solid fa-flag"></i>  Home
+            </Nav.Link>
+            <Nav.Link>
+                <i class="fa-solid fa-arrow-right-to-bracket"></i>  Sign in
+            </Nav.Link>
+            <Nav.Link>
+                <i class="fa-solid fa-plus"></i>  Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
