7b0d9b88bd67cee7dbe1f86d6e634741822fb619
Rhesmika <Rhesmika@gmail.com>
2023-02-21 15:57:25 +0000

    set up axios

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from "../../styles/SignInUpForm.module.css";
+import appStyles from "../../App.module.css";
+
+const SignUpForm = () => {
+    return(
+        <div>
+        <h1>signup form</h1>
+        </div>
+    )
+}
+export default SignUpForm;
