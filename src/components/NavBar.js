30d1bdd4693c6b14b008fe012fa6288a43c17e78
Rhesmika <Rhesmika@gmail.com>
2023-09-23 22:48:14 +0000

    address console error s and npm errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import { removeTokenTimestamp } from "../utils/utils";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -16,6 +17,7 @@ const NavBar = () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
+      removeTokenTimestamp();
     } catch (err) {
       console.log(err);
     }
